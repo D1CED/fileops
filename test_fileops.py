3d940edf41df0f4863cb89aa198c506df142a33c
Jannis M. Hoffmann <jannis@fehcom.de>
2019-09-01 12:36:48 +0200

    made test environment independent

--- test_fileops.py
+++ test_fileops.py
@@ -1,4 +1,6 @@
 from unittest import TestCase, main
+from pathlib import Path
+import tempfile
 import fileops
 
 
@@ -8,9 +10,9 @@ class TestFileops(TestCase):
         testdict = {
             'min': ("()", ['(', ')']),
             'std': (
-                "(if (eq (# ../.) 14) () (u $1 /home/jannis/Music/))",
+                "(if (eq (# ../.) 14) () (u $1 /home/my/Music/))",
                 ['(', 'if', '(', 'eq', '(', '#', '../.', ')', '14', ')',
-                 '(', ')', '(', 'u', '$1', '/home/jannis/Music/', ')', ')']
+                 '(', ')', '(', 'u', '$1', '/home/my/Music/', ')', ')']
             ),
             'hard': (
                 "(((((sdagsadg (&ad, (381)) fasdf)) ()",
@@ -31,23 +33,29 @@ class TestFileops(TestCase):
         self.assertEqual(list(data), ["abc", ")", "1"])
 
     def test_parser(self):
-        fileops._indirs = ['.']
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_path = Path(temp_dir.name)
+        (temp_path / "file1").touch()
+        (temp_path / "file2").touch()
+
+        fileops._indirs = [str(temp_path)]
         tests = [
             ("()", None),
             ("(eq 14 20)", False),
             ("(eq 3 3)", True),
-            ("(# .)", 2),
+            (f"(# {temp_path!s})", 2),
             ("(# $1)", 2),
-            ("(u . .)", {'fileops.py', 'test_fileops.py'}),
-            ("(d . .)", False),
-            ("(in . .)", True),
-            ("(if (in . .) (eq 2 (# $1)) ())", True),
+            (f"(u {temp_path!s} {temp_path!s})", {'file1', 'file2'}),
+            (f"(d {temp_path!s} {temp_path!s})", False),
+            (f"(in {temp_path!s} {temp_path!s})", True),
+            (f"(if (in {temp_path!s} {temp_path!s}) (eq 2 (# $1)) ())", True),
             ("(if (lt 14 2) (eq 2 (# $1)) ())", None),
         ]
         for inp, want in tests:
             with self.subTest(inp=inp, want=want):
                 self.assertEqual(fileops.parse(inp), want)
 
+        temp_dir.cleanup()
 
 if __name__ == '__main__':
     main()
